afa7a6b63c3ce6114adace3a781c190459e7f67d
howard127 <kobun127@gmail.com>
2023-08-27 00:07:34 +0900

    [update] change name

--- src/main/kotlin/jp/xhw/noticebox/NoticeBoxPlugin.kt
+++ src/main/kotlin/jp/xhw/noticebox/NoticeBoxPlugin.kt
@@ -1,10 +1,10 @@
-package jp.xhw.howatemplate
+package jp.xhw.noticebox
 
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.logging.Level
 
-class HowaTemplate : JavaPlugin() {
+class NoticeBoxPlugin : JavaPlugin() {
 
     override fun onEnable() {
         Bukkit.getLogger().log(Level.INFO, "Plugin Enabled!")
